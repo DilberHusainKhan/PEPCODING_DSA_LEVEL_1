f614bdee8423a76352637f33a6757327bcc4e885
DilberHusainKhan <dilberjmi@gmail.com>
2022-01-20 22:58:47 +0530

    2D array intro

---
+++ 1_Basic_Of_Programming/4_2D_Array/A_2dArrayDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A_2dArrayDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int[][] arr = new int[r][c];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        } 
+
+        // display
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}

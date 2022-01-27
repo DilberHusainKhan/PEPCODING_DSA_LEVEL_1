4f29384944ec68031f7b988ad837a03d193fe0f4
DilberHusainKhan <dilberjmi@gmail.com>
2022-01-28 00:14:42 +0530

    String Difference of every two consecutive character of a string

---
+++ 1_Basic_Of_Programming/5_String_StringBuilder_and_ArrayList/F_DiffOfTwoConsecutiveChar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class F_DiffOfTwoConsecutiveChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String result = solution(str);
+        System.out.println(result);
+    }
+    public static String solution(String str){
+        StringBuilder sb = new StringBuilder();
+        sb.append(str.charAt(0));
+        for(int i=1;i<str.length();i++){
+            char curr = str.charAt(i);
+            char prev = str.charAt(i-1);
+            int gap = curr-prev;
+            sb.append(gap);
+            sb.append(curr);
+        }
+        return sb.toString();
+    }
+}

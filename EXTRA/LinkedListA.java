32bb62dca86aa8022e800989fd2b477d4ab11aa1
dilberhusainkhan <dilberjmi@gmail.com>
2022-03-28 23:44:07 +0530

    linked list prectice

---
+++ EXTRA/LinkedListA.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class LinkedListA {
+    private static class Node{
+        int data;
+        Node next;
+    }
+    private static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+
+        int size(){
+          return size;
+        }
+
+        void addLast(int val){
+          Node node  = new Node();
+          node.data = val;
+          node.next = null;
+          if(size ==0){
+            head = tail = node;
+          }else{
+          tail.next = node;
+          tail = node;
+          }
+          size++;
+        }
+
+        void display(){
+          for(Node temp = this.head; temp != null;temp = temp.next){
+            System.out.println(temp.data+" ");
+          }
+          System.out.println();
+        }
+    }
+  
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+    
+        String str = br.readLine();
+        while(str.equals("quit") == false){
+          if(str.startsWith("addLast")){
+            int val = Integer.parseInt(str.split(" ")[1]);
+            list.addLast(val);
+          } else if(str.startsWith("size")){
+            System.out.println(list.size());
+          } else if(str.startsWith("display")){
+            list.display();
+          }
+          str = br.readLine();
+        }
+      }
+}
